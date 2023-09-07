cee5c3a5334a22176da3e92c2e4a6768e22073ea
Facundo Marin <facundo.marin@moxit.net>
2023-09-07 13:18:16 -0300

    Initial commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿namespace FizzBuzzTDD
+{
+    public class FizzBuzz
+    {
+        public string Obtener(int valor)
+        {
+            // if (valor % 15 == 0) return "fizzbuzz";
+            // if (valor % 3 == 0) return "fizz";
+            // if (valor % 5 == 0) return "buzz";
+            //
+            // return valor.ToString();
+
+            var multiplos = new Dictionary<int, string>()
+            {
+                { 3, "fizz" },
+                { 5, "buzz" }
+            };
+            var output = "";
+
+            foreach (var (multiplo, word) in multiplos)
+            {
+                if (valor % multiplo == 0)
+                {
+                    output += word;
+                }
+            }
+
+            return output == "" ? valor.ToString() : output;
+        }
+    }
+}
---
+++ FizzBuzzXUnitTest/FizzBuzzXUnitTests.cs
@@ -0,0 +1,89 @@
+﻿using FizzBuzzTDD;
+namespace FizzBuzzXUnitTest
+{
+    //Escribir una funcion que al pasarle un numero:
+    //Muestra "fizz" si es multiplo de 3
+    //Muestra "buzz" si es multiplo de 5
+    //muestra "fizzbuzz" si es multiplo de 3 y 5
+    //Muestra el numero si no es multiplo de ninguno de los anteriores
+    public class FizzBuzzXUnitTests
+    {
+        private FizzBuzz fizzBuzz;
+
+        public FizzBuzzXUnitTests()
+        {
+            fizzBuzz = new FizzBuzz();
+        }
+        [Fact]
+        public void fizzbuzz_InputNumber1_Returns1()
+        {
+            var valor = 1;
+            var resultado = fizzBuzz.Obtener(valor);
+
+            Assert.Equal("1" , resultado);
+        }
+
+        [Fact]
+        public void fizzbuzz_InputNumber2_Returns2()
+        {
+            var valor = 2;
+            var resultado = fizzBuzz.Obtener(valor);
+
+            Assert.Equal("2" , resultado);
+        }
+
+        [Fact]
+        public void fizzbuzz_IntputNumber3_RetornsFizz()
+        {
+            var valor = 3;
+            var resultado = fizzBuzz.Obtener(valor);
+
+            Assert.Equal("fizz" , resultado);
+        }
+
+        // [Fact]
+        // public void fizzbuzz_InputNumber4_Returns4()
+        // {
+        //     var valor = 4;
+        //     var resultado = fizzBuzz.Obtener(valor);
+        //
+        //     Assert.Equal("4", resultado);
+        // }
+
+        [Theory]
+        [InlineData(6, "fizz")]
+        [InlineData(9, "fizz")]
+        [InlineData(12, "fizz")]
+        public void fizzbuzz_InputMultiplosDeTres_Returnsfizz(int valor, string expectedResult)
+        {
+            var resultado = fizzBuzz.Obtener(valor);
+
+            Assert.Equal(expectedResult, resultado);
+        }
+
+        [Fact]
+        public void fizzbuzz_InputNumber5_Returnsbuzz()
+        {
+            var result = fizzBuzz.Obtener(5);
+
+            Assert.Equal("buzz", result);
+        }
+
+        [Theory]
+        [InlineData(10, "buzz")]
+        [InlineData(20, "buzz")]
+        public void fizzbuzz_InputMultiplosDeCinco_Returnsbuzz(int valor, string expectedResult)
+        {
+            var resultado = fizzBuzz.Obtener(valor);
+
+            Assert.Equal(expectedResult, resultado);
+        }
+
+        [Fact]
+        public void fizzbuzz_InputMultiploDeTresYCinco_Returnsfizzbuzz()
+        {
+            var resultado = fizzBuzz.Obtener(15);
+            Assert.Equal("fizzbuzz", resultado);
+        }
+    }
+}
